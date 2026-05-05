93c15d10d9a206bca8052b5ae4cebdc7ad403464
Achintya Rai <143393443+raiyashu2004@users.noreply.github.com>
2026-05-05 15:59:04 +0530

    Delete src/main directory

--- src/main/java/com/fraud/detection/repository/BlockedIPRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fraud.detection.repository;
-
-import com.fraud.detection.model.BlockedIP;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface BlockedIPRepository extends JpaRepository<BlockedIP, Long> {
-    Optional<BlockedIP> findByIpAddress(String ipAddress);
-}
